5292aca5f11c3337eb05eae0c0afa63c7ef8ee9b
Carlos Castañeda <carlos.castaneda.gutierrez@gmail.com>
2021-06-01 01:06:50 -0500

    Fix next/Link to next/link

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Image from 'next/image'
 
 const Navbar = () => {
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/Link'
+import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
